6f7bfb0e9f9cbad47bca43545a48d4bfd5c3079d
rezahy <reza1997hy@yahoo.com>
2025-04-08 09:46:37 +0430

    fix hover effect on mobile device

--- src/components/news.tsx
+++ src/components/news.tsx
@@ -48,7 +48,7 @@ const News = ({ article }: NewsProps) => {
 					className="h-full object-cover w-full group-hover:scale-115 transition-all duration-500"
 				/>
 				<Button
-					className="scale-0 opacity-0  cursor-pointer absolute top-2 right-2  dark:bg-black/50  dark:hover:bg-black/75 group-hover:scale-100 group-hover:opacity-100 duration-300"
+					className="xl:scale-0 xl:opacity-0  cursor-pointer absolute top-2 right-2  dark:bg-black/50  dark:hover:bg-black/75 group-hover:scale-100 group-hover:opacity-100 duration-300"
 					variant="outline"
 					size="icon"
 					disabled={isPending}
